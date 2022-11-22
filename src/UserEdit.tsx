dbdb343b11a1c1c2cc71a3db20a5efef51f8cc6f
Ilia Burakov <burakov.ilia@gmail.com>
2022-11-23 00:31:26 +0800

    init

---
+++ src/UserEdit.tsx
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import { AutocompleteInput, Edit, ReferenceInput, Show, SimpleForm, Tab, TabbedShowLayout, TabbedShowLayoutTabs } from 'react-admin';
+
+export const createSearchFilter = (searchText: string) => ({ q: searchText });
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function valueToId(value: any): number {
+    if (value === null || value === undefined) {
+        return value;
+    }
+    if (typeof value === 'number') {
+        return value;
+    }
+    if (typeof value === 'string') {
+        return Number(value);
+    }
+    if (typeof value === 'object' && Object.hasOwn(value, 'id')) {
+        return value.id;
+    }
+    // eslint-disable-next-line no-console
+    console.error(`Can not transform this value to id: ${value}`);
+    return -1;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function valueToIdObject(value: any) {
+    if (value === null || value === undefined) {
+        return value;
+    }
+    if (typeof value === 'number') {
+        return { id: value };
+    }
+    if (typeof value === 'string') {
+        return { id: Number(value) };
+    }
+    if (typeof value === 'object' && Object.hasOwn(value, 'id')) {
+        return { id: value.id };
+    }
+    // eslint-disable-next-line no-console
+    console.error(`Can not transform this value to object: ${value}`);
+    return { id: -1 };
+}
+
+export const UserEdit = () => {
+  return (
+      <Edit>
+          <SimpleForm>
+              <ReferenceInput label="Country" source="country" parse={valueToIdObject} format={valueToId} reference="country" sort={{ field: 'name', order: 'ASC' }} required fullWidth>
+                  <AutocompleteInput optionText="name" filterToQuery={createSearchFilter} parse={valueToIdObject} format={valueToId} fullWidth />
+              </ReferenceInput>
+          </SimpleForm>
+      </Edit>
+  );
+};
---
+++ src/fakeData/reproDataProvider.ts
@@ -0,0 +1,7 @@
+import fakeDataStorage from './fakeDataStorage.json';
+import fakeDataProvider from 'ra-data-fakerest';
+
+export const fakeJsonDataProvider = fakeDataProvider({
+    user: fakeDataStorage.user,
+    country: fakeDataStorage.country,
+});
